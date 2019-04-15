1b78c6190e49955a6f16b3c173f5b5883a5edf2d
baiyongxiao <1176632330@qq.com>
2019-04-15 22:12:43 +0800

    保险产品投票系统vue代码

---
+++ src/router/index.js
@@ -0,0 +1,81 @@
+import en from '../i18n/lang/en'
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/views/login/index'
+import Layout from '@/views/layout/layout'
+import HomeMain from '@/views/index/mainIndex'
+
+// 不是必须加载的组件使用懒加载
+const NotFound = () => import('@/page404')
+const AdminSetting = () => import('@/views/syssetting/adminSetting')
+
+Vue.use(Router)
+let routeNmae = en.routeNmae
+let defaultRouter = [
+  { path: '/',
+    redirect: '/index',
+    hidden: true,
+    children: []
+  },
+  {
+    path: '/login',
+    component: Login,
+    name: '',
+    hidden: true,
+    children: []
+  },
+  {
+    path: '/index',
+    iconCls: 'fa fa-dashboard', // 图标样式class
+    name: routeNmae.home,
+    component: Layout,
+    alone: true,
+    children: [
+      {
+        path: '/index',
+        iconCls: 'fa fa-dashboard', // 图标样式class
+        name: '主页',
+        component: HomeMain,
+        children: []
+      }
+    ]
+  },
+  {
+    path: '/404',
+    component: NotFound,
+    name: '404',
+    hidden: true,
+    children: []
+  },
+]
+
+let addRouter = [
+
+  {
+    path: '/adminSetting',
+    iconCls: 'el-icon-setting', // 图标样式class
+    name: routeNmae.systemSettings,
+    component: Layout,
+    alone: true,
+    meta: {role: ['superAdmin']},
+    children: [
+      {
+        path: '/adminSetting',
+        iconCls: 'el-icon-menu', // 图标样式class
+        name: routeNmae.adminSetting,
+        component: AdminSetting,
+        children: []
+      },
+    ]
+  },
+  { path: '*',
+    redirect: '/404',
+    hidden: true,
+    children: []
+  },
+
+]
+export default new Router({
+  routes: defaultRouter
+})
+export {defaultRouter, addRouter}
